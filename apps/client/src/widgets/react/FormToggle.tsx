fd36e33c481784482cc35a08f6bb4fddd9e58f6a
Ian Sherr <ian@timeworthymedia.com>
2026-08-20 11:32:37 -0400

    refactor(ui): restore core FormToggle styles to keep core clean for separate plugin

--- apps/client/src/widgets/react/FormToggle.tsx
+++ apps/client/src/widgets/react/FormToggle.tsx
@@ -35,9 +35,6 @@ export default function FormToggle({ currentValue, helpPage, switchOnName, switc
         <div className="switch-widget">
             {hasLabel && <span className="switch-name">{ currentValue ? switchOffName : switchOnName }</span>}
             { afterName }
-            <span className={clsx("switch-status-pill", currentValue ? "on" : "off")}>
-                {currentValue ? "ON" : "OFF"}
-            </span>
 
             <label>
                 <div
